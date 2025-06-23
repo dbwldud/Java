338ed477fe188ca21ca27f3736482754c2eafbfe
dbwldud <jiyeongyu576@gmail.com>
2025-06-23 14:26:18 +0900

    ..

--- src/a5_class/Order.java
+++ src/a5_class/Order.java
@@ -20,7 +20,11 @@ public class Order {
         this.orderDate = orderDate;
         this.totalAmount = totalAmount;
     }
+    
     // 메서드
     // 주문수량 입력
-    setTotalAmount
+    public double setTotalAmount(double newAmount) {
+        this.totalAmount = newAmount;
+        return this.totalAmount;
+    }
 }
