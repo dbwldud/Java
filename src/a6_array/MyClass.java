ebc1f9e0463ff3bd1422909b81ac3b87c59296de
dbwldud <jiyeongyu576@gmail.com>
2025-06-25 13:40:51 +0900

    ..

--- src/a6_array/MyClass.java
+++ src/a6_array/MyClass.java
@@ -8,6 +8,11 @@ public class MyClass {
         double perimeter = r.getPerimeter();
         System.out.println("둘레는 " + perimeter); // 10
         // 크기 재설정
+//        double[] array = new double[2];
+//        array[0] = 4;
+//        array[1] = 5;
+//        r.resize(array);
+//        double[] array2 = new double[] {4,5};
         r.resize(new double[] {4,5});
         area = r.getArea();
         System.out.println("면적은 " + area); // 20
---
+++ src/a6_array/Test.java
@@ -0,0 +1,7 @@
+package a6_array;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
