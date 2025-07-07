a079da065082b090f8b26436f11c1981b1aca201
dbwldud <jiyeongyu576@gmail.com>
2025-07-07 17:28:40 +0900

    Update GameManager.java

--- src/a10_interface_abstract/GameManager.java
+++ src/a10_interface_abstract/GameManager.java
@@ -35,42 +35,26 @@ public class GameManager {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("첫번째 몬스터 : ");
-        Monster slime = monsters.get(scanner.nextInt());
+        Monster first = monsters.get(scanner.nextInt());
         System.out.println("두번째 몬스터 : ");
-        Monster orc = monsters.get(scanner.nextInt());
-        System.out.println("세번째 몬스터 : ");
-        Monster goblin = monsters.get(scanner.nextInt());
-        System.out.println("네번째 몬스터 : ");
-        Monster skeleton = monsters.get(scanner.nextInt());
-        System.out.println("다섯번째 몬스터 : ");
-        Monster troll = monsters.get(scanner.nextInt());
-        System.out.println("여섯번째 몬스터 : ");
-        Monster golem = monsters.get(scanner.nextInt());
-        System.out.println("일곱번째 몬스터 : ");
-        Monster wyvern = monsters.get(scanner.nextInt());
-        System.out.println("여덟번째 몬스터 : ");
-        Monster lich = monsters.get(scanner.nextInt());
-        System.out.println("아홉번째 몬스터 : ");
-        Monster chimera = monsters.get(scanner.nextInt());
-        System.out.println("열번째 몬스터 : ");
-        Monster dragon = monsters.get(scanner.nextInt());
+        Monster second = monsters.get(scanner.nextInt());
 
         int turn = 1;
 
-        while (slime.getHp() > 0 && orc.getHp() > 0) {
+        while (first.getHp() > 0 && second.getHp() > 0) {
             System.out.println("n==== 턴 " + turn + " ====");
 
-            int damageToSlime = Math.max(0, orc.getAttack() - slime.getDefense());
-            slime.setHp(Math.max(slime.getHp() - damageToSlime, 0));
-            System.out.println(orc.getName() + "이(가) " + slime.getName() + "을(를) 공격! 피해: " + damageToSlime + ", 남은 HP: " + slime.getHp());
+            int damageToSlime = Math.max(0, second.getAttack() - first.getDefense());
+            first.setHp(Math.max(first.getHp() - damageToSlime, 0));
+            System.out.println(second.getName() + "이(가) " + first.getName() + "을(를) 공격! 피해: " + damageToSlime + ", 남은 HP: " + first.getHp());
 
             // 만약에 슬라임이 이미 hp가 없으면 오크를 공격할 수 없어야 함
-            if (slime.getHp() <= 0)
+            if (first.getHp() <= 0)
                 break;
 
-            int damageToOrc = Math.max(0, slime.getAttack() - orc.getDefense());
-            orc.setHp(Math.max(orc.getHp() - damageToOrc, 0));
-            System.out.println(slime.getName() + "이(가) " + orc.getName() + "을(를) 공격! 피해: " + damageToOrc + ", 남은 HP: " + orc.getHp());
+            int damageToOrc = Math.max(0, first.getAttack() - second.getDefense());
+            second.setHp(Math.max(second.getHp() - damageToOrc, 0));
+            System.out.println(first.getName() + "이(가) " + second.getName() + "을(를) 공격! 피해: " + damageToOrc + ", 남은 HP: " + second.getHp());
 
             turn++;
 
@@ -86,8 +70,8 @@ public class GameManager {
 
         System.out.println();
         System.out.println("최종승자는 : ");
-        if (orc.getHp() > 0)
-            orc.info();
-        else slime.info();
+        if (second.getHp() > 0)
+            second.info();
+        else first.info();
     }
 }
