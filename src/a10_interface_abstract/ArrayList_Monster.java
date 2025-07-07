72c9c73461315b209d5a2068a937c493bed0b40e
dbwldud <jiyeongyu576@gmail.com>
2025-07-07 17:25:07 +0900

    ..

--- src/a10_interface_abstract/ArrayList_Monster.java
+++
@@ -1,7 +0,0 @@
-package a10_interface_abstract;
-
-public class ArrayList_Monster {
-    public static void main(String[] args) {
-        
-    }
-}
--- src/a10_interface_abstract/GameManager.java
+++ src/a10_interface_abstract/GameManager.java
@@ -1,9 +1,59 @@
 package a10_interface_abstract;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class GameManager {
     public static void main(String[] args) {
-        Monster slime = new Monster(3, 8, 30, "슬라임");
-        Monster orc = new Monster(5, 12, 40, "오크");
+//        Monster slime = new Monster(3, 8, 30, "슬라임");
+//        Monster orc = new Monster(5, 12, 40, "오크");
+//        Monster goblin = new Monster(4, 12, 50, "고블린");
+//        Monster skeleton = new Monster(10, 14, 60, "스켈레톤");
+//        Monster troll = new Monster(6, 18, 120, "트롤");
+//        Monster golem = new Monster(25, 20, 100, "골렘");
+//        Monster wyvern = new Monster(15, 25, 150, "와이번");
+//        Monster lich = new Monster(12, 35, 130, "리치");
+//        Monster chimera = new Monster(20, 30, 200, "키메라");
+//        Monster dragon = new Monster(30, 40, 300, "드래곤");
+
+        List<Monster> monsters = new ArrayList<Monster>();
+        monsters.add(new Monster(3, 8, 30, "슬라임"));
+        monsters.add(new Monster(5, 12, 40, "오크"));
+        monsters.add(new Monster(4, 12, 50, "고블린"));
+        monsters.add(new Monster(10, 14, 60, "스켈레톤"));
+        monsters.add(new Monster(6, 18, 120, "트롤"));
+        monsters.add(new Monster(25, 20, 100, "골렘"));
+        monsters.add(new Monster(15, 25, 150, "와이번"));
+        monsters.add(new Monster(12, 35, 130, "리치"));
+        monsters.add(new Monster(20, 30, 200, "키메라"));
+        monsters.add(new Monster(30, 40, 300, "드래곤"));
+
+        for (int i = 0; i <= 9; i++) {
+            System.out.println(i + " : " + monsters.get(i).getName());
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("첫번째 몬스터 : ");
+        Monster slime = monsters.get(scanner.nextInt());
+        System.out.println("두번째 몬스터 : ");
+        Monster orc = monsters.get(scanner.nextInt());
+        System.out.println("세번째 몬스터 : ");
+        Monster goblin = monsters.get(scanner.nextInt());
+        System.out.println("네번째 몬스터 : ");
+        Monster skeleton = monsters.get(scanner.nextInt());
+        System.out.println("다섯번째 몬스터 : ");
+        Monster troll = monsters.get(scanner.nextInt());
+        System.out.println("여섯번째 몬스터 : ");
+        Monster golem = monsters.get(scanner.nextInt());
+        System.out.println("일곱번째 몬스터 : ");
+        Monster wyvern = monsters.get(scanner.nextInt());
+        System.out.println("여덟번째 몬스터 : ");
+        Monster lich = monsters.get(scanner.nextInt());
+        System.out.println("아홉번째 몬스터 : ");
+        Monster chimera = monsters.get(scanner.nextInt());
+        System.out.println("열번째 몬스터 : ");
+        Monster dragon = monsters.get(scanner.nextInt());
 
         int turn = 1;
 
