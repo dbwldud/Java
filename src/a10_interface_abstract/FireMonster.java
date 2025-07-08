b338a4433560561994601884b0acd1fee2d66b7d
dbwldud <jiyeongyu576@gmail.com>
2025-07-08 16:35:25 +0900

    ..

---
+++ src/a10_interface_abstract/FireMonster.java
@@ -0,0 +1,12 @@
+package a10_interface_abstract;
+
+import java.lang.reflect.Parameter;
+
+public class FireMonster {
+    Parameter fireSkillDamage;
+    Parameter name;
+    Parameter hp;
+    Parameter attack;
+    Parameter defense;
+    
+}
