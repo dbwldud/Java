c019434dab2f94e709f7964153f221ebdd94c26d
dbwldud <jiyeongyu576@gmail.com>
2025-07-17 17:30:08 +0900

    ..

---
+++ src/a10_interface_abstract/Hellhound.java
@@ -0,0 +1,4 @@
+package a10_interface_abstract;
+
+public class Hellhound {
+}
--- src/a10_interface_abstract/Troll.java
+++ src/a10_interface_abstract/Troll.java
@@ -1,4 +1,13 @@
 package a10_interface_abstract;
 
-public class Troll {
+public class Troll extends NormalMonster implements Healable {
+    public Troll(String name, int hp, int attack, int defense) {
+        super(name, hp, attack, defense);
+    }
+
+    // Healble 인터페이스의 규칙에 따라 heal() 메서드를 구현
+    @Override
+    public void heal() {
+        // 최대 체력의 10%를 회복함
+    }
 }
