933922dc56cfecd2a666bbe2217b99f2949347ed
dbwldud <jiyeongyu576@gmail.com>
2025-07-18 17:24:32 +0900

    ..

--- src/a10_interface_abstract/Hellhound.java
+++ src/a10_interface_abstract/Hellhound.java
@@ -1,4 +1,16 @@
 package a10_interface_abstract;
 
-public class Hellhound {
+public class Hellhound extends FireMonster implements Healable {
+
+    public Hellhound(String name, int hp, int attack, int defense, int fireSkillDamage) {
+        super(name, hp, attack, defense, fireSkillDamage);
+    }
+    
+    Monster hellhound = new Monster("헬하운드", 90, 28, 10, 25);
+
+    // Healable 인터페이스의 규칙에 따라 heal() 메서드를 구현
+    @Override
+    public  void heal() {
+        // 최대 체력의 20%를 회복함
+    }
 }
--- src/a10_interface_abstract/Troll.java
+++ src/a10_interface_abstract/Troll.java
@@ -1,13 +1,19 @@
 package a10_interface_abstract;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Troll extends NormalMonster implements Healable {
-    public Troll(String name, int hp, int attack, int defense) {
+    public Troll(int name, int hp, int attack, String defense) {
         super(name, hp, attack, defense);
     }
 
-    // Healble 인터페이스의 규칙에 따라 heal() 메서드를 구현
+    Monster troll = new Monster(6, 18, 120, "트롤");
+
+    // Healable 인터페이스의 규칙에 따라 heal() 메서드를 구현
     @Override
     public void heal() {
         // 최대 체력의 10%를 회복함
+
     }
 }
